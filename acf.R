305d588df2ed42ab84ece7f91ef77ae1c36abd53
Clark Fitzgerald <clarkfitzg@gmail.com>
2020-03-05 14:18:41 -0800

    helper functions

--- acf.R
+++ acf.R
@@ -1,31 +1,45 @@
-d = read.csv("IP21.3-29-17.PreInjection.Standard.Cylinder.2017-03-29_17-13-40.SE16.nse.converted.SpikeSortedForAutocorrelation.csv"
-, header = FALSE
-, col.names = c("index", "time", "fire"))
+# Read a single spike train file from a zip file that contains many such files.
+read_one_spike_train_file = function(fname, zipfile = "data.zip"
+    , col.names = c("index", "time", "spike"), ...)
+{
+    f = unz("data.zip", fname)
+    d = read.table(f, header = FALSE, col.names = col.names, ...)
+    
+    time_deltas = table(diff(d$time))
+    if(length(time_deltas) > 1)
+        stop("Time intervals are not all the same.")
 
-n = dim(d)[1]
+    n = dim(d)[1]
+    # Assuming observations are at every millisecond.
+    minutes = n / (60 * 1000)
+    message(sprintf("Recording period was %g minutes.", minutes))
 
-# If these observations are every milliseconds then the data spans 8 minutes:
-minutes = n / (60 * 1000)
+    d
+}
 
-# Verify that measurements are at regular timestamps:
-table(diff(d$time))
-# Yes.
 
-a = acf(d$fire, lag.max = 800)
+plot_acf = function(d, lag.max = 100, ...){
+    # Drop the first observation that has ACF 1.
+    a = acf(d$spike, lag.max = lag.max, plot = FALSE)
+    a = a$acf[-1]
+    plot(a
+        , type = "l"
+        , xlab = "lag"
+        , ylab = "autocorrelation"
+        , ...
+    )
+}
 
-a2 = a$acf[-1]
 
-# We see some negative correlation for the first 5 periods, consistent with the neuron not firing twice within 5 ms.
-# After that it's a decaying positive correlation.
-pdf("acf800.pdf")
-plot(seq(-799, 800), c(rev(a2), a2)
-    , type = "l"
-    , xlab = "lag"
-    , ylab = "autocorrelation"
-)
-dev.off()
+# Compare ACF before and after injection
 
-plot(a2[1:60], type = "l")
+PreInjection = read_one_spike_train_file("IP18.12-9-16.PreInjection.Standard.Cylinder.2016-12-09_14-31-21.SE9.nse.converted.SpikeSortedForAutocorrelation")
 
-# Not showing much difference.
-plot(a2[1:400], type = "l")
+PostInjection = read_one_spike_train_file("IP18.12-9-16.PostInjection.Standard.Cylinder.2016-12-09_19-18-01.SE15.nse.converted.SpikeSortedForAutocorrelation")
+
+
+# The pre injection ACF is around 0.002, while the post injection ACF is around 0.01, around five times larger.
+# What could that mean?
+par(mfrow = c(2, 1))
+plot_acf(PreInjection, main = "Pre Injection")
+plot_acf(PostInjection, main = "Post Injection")
